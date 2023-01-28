b8b42b39b7e7fa4671d10ad09c7d70d97df31891
Hrithik Thakur <H.Thakur@sms.ed.ac.uk>
2023-01-28 22:27:58 +0000

    Stage 1 complete

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,7 +1,18 @@
 package search;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        String[] words = scanner.nextLine().split(" ");
+        String target = scanner.nextLine();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(target)) {
+                System.out.println(i + 1);
+                return;
+            }
+        }
+        System.out.println("Not found");
     }
 }
